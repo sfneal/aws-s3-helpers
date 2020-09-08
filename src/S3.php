0e6b7b68205b98e41b225f9a95440f34453fe3f2
Stephen Neal <stephen.neal14@gmail.com>
2020-09-08 12:40:32 -0400

    ADD use of S3 class

---
+++ src/S3.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Sfneal\Helpers\Aws\S3;
+
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Storage;
+
+
+class S3
+{
+    /**
+     * @var string
+     */
+    private $s3_key;
+
+    /**
+     * S3 constructor.
+     *
+     * @param string $s3_key
+     */
+    public function __construct(string $s3_key)
+    {
+        $this->s3_key = $s3_key;
+    }
+
+    /**
+     * Return either an S3 file url or a local file url
+     *
+     * @param bool $temp
+     * @return mixed
+     */
+    public function url(bool $temp = true) {
+        if ($temp) {
+            return Storage::disk('s3')->temporaryUrl($this->s3_key, now()->addMinutes(60));
+        } else {
+            return Storage::disk('s3')->url($this->s3_key);
+        }
+    }
+
+    /**
+     * Determine if an S3 file exists
+     *
+     * @return bool
+     */
+    public function exists() {
+        return Storage::disk('s3')->exists($this->s3_key);
+    }
+
+    /**
+     * Upload a file to an S3 bucket
+     *
+     * @param $file_path
+     * @param string|null $acl
+     * @return mixed
+     */
+    public function upload(string $file_path, string $acl = null) {
+        if(is_null($acl)) {
+            Storage::disk('s3')->put($this->s3_key, fopen($file_path, 'r+'));
+        } else {
+            Storage::disk('s3')->put($this->s3_key, fopen($file_path, 'r+'), $acl);
+        }
+        return $this->url();
+    }
+
+    /**
+     * Upload raw file contents to an S3 bucket
+     *
+     * @param string $file_contents
+     * @param string|null $acl
+     * @return mixed
+     */
+    function upload_raw(string $file_contents, string $acl = null) {
+        if(is_null($acl)) {
+            Storage::disk('s3')->put($this->s3_key, $file_contents);
+        } else {
+            Storage::disk('s3')->put($this->s3_key, $file_contents, $acl);
+        }
+        return $this->url();
+    }
+
+    /**
+     * Download a file from an S3 bucket
+     *
+     * @param string|null $file_name
+     * @return \Illuminate\Http\Response
+     * @throws FileNotFoundException
+     */
+    public function download(string $file_name = null) {
+        if (is_null($file_name)) {
+            $file_name = basename($this->s3_key);
+        }
+
+        $mime = Storage::disk('s3')->getMimetype($this->s3_key);
+        $size = Storage::disk('s3')->getSize($this->s3_key);
+
+        $response = [
+            'Content-Type' => $mime,
+            'Content-Length' => $size,
+            'Content-Description' => 'File Transfer',
+            'Content-Disposition' => "attachment; filename={$file_name}",
+            'Content-Transfer-Encoding' => 'binary',
+        ];
+
+        return Response::make(Storage::disk('s3')->get($this->s3_key), 200, $response);
+    }
+
+    /**
+     * Delete a file or folder from an S3 bucket
+     *
+     * @return bool
+     */
+    public function delete(): bool {
+        return Storage::disk('s3')->delete($this->s3_key);
+    }
+
+    /**
+     * List all of the files in an S3 directory
+     *
+     * @return array
+     */
+    public function list()
+    {
+        $storage = Storage::disk('s3');
+        $client = $storage->getAdapter()->getClient();
+        $command = $client->getCommand('ListObjects');
+        $command['Bucket'] = $storage->getAdapter()->getBucket();
+        $command['Prefix'] = $this->s3_key;
+        $result = $client->execute($command);
+
+        $files = array();
+        if (isset($result['Contents']) && !empty($result['Contents'])) {
+            foreach ($result['Contents'] as $content) {
+                $url = fileURL($content['Key']);
+                $parts = explode('/', explode('?', $url, 2)[0]);
+                $files[] = [
+                    'name' => end($parts),
+                    'url' => $url,
+                    'key' => $content['Key']
+                ];
+            }
+        }
+        return $files;
+    }
+}
--- src/s3-helpers.php
+++ src/s3-helpers.php
@@ -1,23 +1,18 @@
 <?php
 
 use Illuminate\Contracts\Filesystem\FileNotFoundException;
-use Illuminate\Support\Facades\Response;
-use Illuminate\Support\Facades\Storage;
+use Sfneal\Helpers\Aws\S3\S3;
 
 
 /**
  * Return either an S3 file url or a local file url
  *
- * @param $path
+ * @param string $path
  * @param bool $temp
  * @return mixed
  */
-function fileURL($path, $temp=true) {
-    if ($temp) {
-        return Storage::disk('s3')->temporaryUrl($path, now()->addMinutes(60));
-    } else {
-        return Storage::disk('s3')->url($path);
-    }
+function fileURL(string $path, bool $temp = true) {
+    return (new S3($path))->url($temp);
 }
 
 
@@ -28,7 +23,7 @@ function fileURL($path, $temp=true) {
  * @return bool
  */
 function s3_exists(string $s3_key) {
-    return Storage::disk('s3')->exists($s3_key);
+    return (new S3($s3_key))->exists();
 }
 
 
@@ -41,12 +36,7 @@ function s3_exists(string $s3_key) {
  * @return mixed
  */
 function s3_upload($s3_key, $file_path, $acl=null) {
-    if(is_null($acl)) {
-        Storage::disk('s3')->put($s3_key, fopen($file_path, 'r+'));
-    } else {
-        Storage::disk('s3')->put($s3_key, fopen($file_path, 'r+'), $acl);
-    }
-    return fileURL($s3_key);
+    return (new S3($s3_key))->upload($file_path, $acl);
 }
 
 
@@ -54,27 +44,12 @@ function s3_upload($s3_key, $file_path, $acl=null) {
  * Download a file from an S3 bucket
  *
  * @param $file_url
- * @param null $file_name
+ * @param string|null $file_name
  * @return \Illuminate\Http\Response
  * @throws FileNotFoundException
  */
-function s3_download($file_url, $file_name=null) {
-    if (is_null($file_name)) {
-        $file_name = basename($file_url);
-    }
-
-    $mime = Storage::disk('s3')->getMimetype($file_url);
-    $size = Storage::disk('s3')->getSize($file_url);
-
-    $response = [
-        'Content-Type' => $mime,
-        'Content-Length' => $size,
-        'Content-Description' => 'File Transfer',
-        'Content-Disposition' => "attachment; filename={$file_name}",
-        'Content-Transfer-Encoding' => 'binary',
-    ];
-
-    return Response::make(Storage::disk('s3')->get($file_url), 200, $response);
+function s3_download($file_url, string $file_name = null) {
+    return (new S3($file_url))->download($file_name);
 }
 
 
@@ -82,9 +57,11 @@ function s3_download($file_url, $file_name=null) {
  * Delete a file or folder from an S3 bucket
  *
  * @param $s3_key
+ * @return bool
  */
-function s3_delete($s3_key) {
-    Storage::disk('s3')->delete($s3_key);
+function s3_delete($s3_key): bool
+{
+    return (new S3($s3_key))->delete();
 }
 
 
@@ -92,17 +69,12 @@ function s3_delete($s3_key) {
  * Upload raw file contents to an S3 bucket
  *
  * @param $s3_key
- * @param $file_contents
- * @param null $acl
+ * @param string $file_contents
+ * @param string|null $acl
  * @return mixed
  */
-function s3_upload_raw($s3_key, $file_contents, $acl=null) {
-    if(is_null($acl)) {
-        Storage::disk('s3')->put($s3_key, $file_contents);
-    } else {
-        Storage::disk('s3')->put($s3_key, $file_contents, $acl);
-    }
-    return fileURL($s3_key);
+function s3_upload_raw($s3_key, string $file_contents, string $acl = null) {
+    return (new S3($s3_key))->upload_raw($file_contents, $acl);
 }
 
 
@@ -114,24 +86,5 @@ function s3_upload_raw($s3_key, $file_contents, $acl=null) {
  */
 function s3_list($s3_key)
 {
-    $storage = Storage::disk('s3');
-    $client = $storage->getAdapter()->getClient();
-    $command = $client->getCommand('ListObjects');
-    $command['Bucket'] = $storage->getAdapter()->getBucket();
-    $command['Prefix'] = $s3_key;
-    $result = $client->execute($command);
-
-    $files = array();
-    if (isset($result['Contents']) && !empty($result['Contents'])) {
-        foreach ($result['Contents'] as $content) {
-            $url = fileURL($content['Key']);
-            $parts = explode('/', explode('?', $url, 2)[0]);
-            $files[] = [
-                'name' => end($parts),
-                'url' => $url,
-                'key' => $content['Key']
-            ];
-        }
-    }
-    return $files;
+    return (new S3($s3_key))->list();
 }
