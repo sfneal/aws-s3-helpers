af3cc111b38c58183621733d2ea1feb1c23fc8d4
Stephen Neal <stephen.neal14@gmail.com>
2020-09-08 12:41:09 -0400

    Apply fixes from StyleCI (#2)

--- src/S3.php
+++ src/S3.php
@@ -6,7 +6,6 @@ use Illuminate\Contracts\Filesystem\FileNotFoundException;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Storage;
 
-
 class S3
 {
     /**
@@ -25,12 +24,13 @@ class S3
     }
 
     /**
-     * Return either an S3 file url or a local file url
+     * Return either an S3 file url or a local file url.
      *
      * @param bool $temp
      * @return mixed
      */
-    public function url(bool $temp = true) {
+    public function url(bool $temp = true)
+    {
         if ($temp) {
             return Storage::disk('s3')->temporaryUrl($this->s3_key, now()->addMinutes(60));
         } else {
@@ -39,54 +39,60 @@ class S3
     }
 
     /**
-     * Determine if an S3 file exists
+     * Determine if an S3 file exists.
      *
      * @return bool
      */
-    public function exists() {
+    public function exists()
+    {
         return Storage::disk('s3')->exists($this->s3_key);
     }
 
     /**
-     * Upload a file to an S3 bucket
+     * Upload a file to an S3 bucket.
      *
      * @param $file_path
      * @param string|null $acl
      * @return mixed
      */
-    public function upload(string $file_path, string $acl = null) {
-        if(is_null($acl)) {
+    public function upload(string $file_path, string $acl = null)
+    {
+        if (is_null($acl)) {
             Storage::disk('s3')->put($this->s3_key, fopen($file_path, 'r+'));
         } else {
             Storage::disk('s3')->put($this->s3_key, fopen($file_path, 'r+'), $acl);
         }
+
         return $this->url();
     }
 
     /**
-     * Upload raw file contents to an S3 bucket
+     * Upload raw file contents to an S3 bucket.
      *
      * @param string $file_contents
      * @param string|null $acl
      * @return mixed
      */
-    function upload_raw(string $file_contents, string $acl = null) {
-        if(is_null($acl)) {
+    public function upload_raw(string $file_contents, string $acl = null)
+    {
+        if (is_null($acl)) {
             Storage::disk('s3')->put($this->s3_key, $file_contents);
         } else {
             Storage::disk('s3')->put($this->s3_key, $file_contents, $acl);
         }
+
         return $this->url();
     }
 
     /**
-     * Download a file from an S3 bucket
+     * Download a file from an S3 bucket.
      *
      * @param string|null $file_name
      * @return \Illuminate\Http\Response
      * @throws FileNotFoundException
      */
-    public function download(string $file_name = null) {
+    public function download(string $file_name = null)
+    {
         if (is_null($file_name)) {
             $file_name = basename($this->s3_key);
         }
@@ -106,16 +112,17 @@ class S3
     }
 
     /**
-     * Delete a file or folder from an S3 bucket
+     * Delete a file or folder from an S3 bucket.
      *
      * @return bool
      */
-    public function delete(): bool {
+    public function delete(): bool
+    {
         return Storage::disk('s3')->delete($this->s3_key);
     }
 
     /**
-     * List all of the files in an S3 directory
+     * List all of the files in an S3 directory.
      *
      * @return array
      */
@@ -128,18 +135,19 @@ class S3
         $command['Prefix'] = $this->s3_key;
         $result = $client->execute($command);
 
-        $files = array();
-        if (isset($result['Contents']) && !empty($result['Contents'])) {
+        $files = [];
+        if (isset($result['Contents']) && ! empty($result['Contents'])) {
             foreach ($result['Contents'] as $content) {
                 $url = fileURL($content['Key']);
                 $parts = explode('/', explode('?', $url, 2)[0]);
                 $files[] = [
                     'name' => end($parts),
                     'url' => $url,
-                    'key' => $content['Key']
+                    'key' => $content['Key'],
                 ];
             }
         }
+
         return $files;
     }
 }
