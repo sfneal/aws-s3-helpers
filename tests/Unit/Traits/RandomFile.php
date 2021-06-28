29765158f2b8d72883461a7eebe8dfbc65c5494d
Stephen Neal <stephen.neal14@gmail.com>
2021-06-28 13:04:45 -0400

    MAKE `UrlTest` class for testing url related methods
    ADD test files.

---
+++ tests/Unit/Traits/RandomFile.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Sfneal\Helpers\Aws\S3\Tests\Unit\Traits;
+
+
+trait RandomFile
+{
+    /**
+     * Retrieve an array of asset files.
+     *
+     * @return array
+     */
+    private static function files(): array
+    {
+        return array_diff(scandir(__DIR__.'/../../Assets'), array('.', '..'));
+    }
+
+    /**
+     * Retrieve a random asset file.
+     *
+     * @return string
+     */
+    public static function randomFile(): string
+    {
+        $files = self::files();
+        print_r($files);
+        $file = array_rand($files);
+        return $files[$file];
+    }
+}
---
+++ tests/Unit/UrlTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Sfneal\Helpers\Aws\S3\Tests\Unit;
+
+
+use Sfneal\Helpers\Aws\S3\StorageS3;
+use Sfneal\Helpers\Aws\S3\Tests\TestCase;
+use Sfneal\Helpers\Aws\S3\Tests\Unit\Traits\RandomFile;
+
+class UrlTest extends TestCase
+{
+    use RandomFile;
+
+    /** @test */
+    public function url_is_valid()
+    {
+        $file = self::randomFile();
+        $url = StorageS3::key($file)->url();
+
+        $this->assertNotNull($url);
+        $this->assertIsString($url);
+
+        // todo: add response testing
+    }
+}
