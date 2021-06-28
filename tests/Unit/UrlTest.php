084a6546ed30728075ca16ee1bb76174dbdbf14f
Stephen Neal <stephen.neal14@gmail.com>
2021-06-28 14:38:57 -0400

    ADD `temp_url_is_valid()` test method

--- tests/Unit/UrlTest.php
+++ tests/Unit/UrlTest.php
@@ -33,5 +33,17 @@ class UrlTest extends TestCase
 
         $this->assertNotNull($url);
         $this->assertIsString($url);
+        $this->assertStringContainsString($this->file, $url);
+    }
+
+    /** @test */
+    public function temp_url_is_valid()
+    {
+        $url = StorageS3::key($this->file)->urlTemp();
+
+        $this->assertNotNull($url);
+        $this->assertIsString($url);
+        $this->assertStringContainsString($this->file, $url);
+        $this->assertNotEquals(StorageS3::key($this->file)->url(), $url);
     }
 }
