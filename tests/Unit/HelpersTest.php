3296dcfb857bdbae04dab39c1a4201cfd7079174
Stephen Neal <stephen.neal14@gmail.com>
2021-06-29 15:59:46 -0400

    MAKE `HelpersTest` for testing helper functions

---
+++ tests/Unit/HelpersTest.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace Sfneal\Helpers\Aws\S3\Tests\Unit;
+
+
+use Sfneal\Helpers\Aws\S3\StorageS3;
+use Sfneal\Helpers\Aws\S3\Tests\StorageS3TestCase;
+
+class HelpersTest extends StorageS3TestCase
+{
+    /** @test */
+    public function file_url_exists()
+    {
+        $url = fileURL($this->file, false);
+
+        $this->assertNotNull($url);
+        $this->assertIsString($url);
+        $this->assertStringContainsString($this->file, $url);
+    }
+
+    /** @test */
+    public function file_url_temp_exists()
+    {
+        $url = fileUrlTemp($this->file);
+
+        $this->assertNotNull($url);
+        $this->assertIsString($url);
+        $this->assertStringContainsString($this->file, $url);
+        $this->assertNotEquals(StorageS3::key($this->file)->url(), $url);
+    }
+
+    /** @test */
+    public function file_exists()
+    {
+        $exists = s3_exists($this->file);
+
+        $this->assertIsBool($exists);
+        $this->assertTrue($exists, "The file '{$this->file}' doesn't exist.");
+    }
+
+    /** @test */
+    public function file_doesnt_exist()
+    {
+        $doesntExist = s3_exists("fake_file_{$this->file}");
+
+        $this->assertIsBool($doesntExist);
+        $this->assertFalse($doesntExist);
+    }
+
+    /** @test */
+    public function directory_exists()
+    {
+        $exists = s3_exists('directory');
+
+        $this->assertIsBool($exists);
+        $this->assertTrue($exists);
+    }
+
+    /** @test */
+    public function directory_doesnt_exist()
+    {
+        $doesntExist = s3_exists('fake_directory');
+
+        $this->assertIsBool($doesntExist);
+        $this->assertFalse($doesntExist);
+    }
+}
