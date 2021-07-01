d2eb8b71e9f68b7289aca236811511e9a4990f81
Stephen Neal <stephen.neal14@gmail.com>
2021-07-01 12:24:13 -0400

    ADD content assertions to `HelpersTest`

--- tests/Unit/HelpersTest.php
+++ tests/Unit/HelpersTest.php
@@ -2,19 +2,30 @@
 
 namespace Sfneal\Helpers\Aws\S3\Tests\Unit;
 
+use GuzzleHttp\Client;
 use Sfneal\Helpers\Aws\S3\StorageS3;
 use Sfneal\Helpers\Aws\S3\Tests\StorageS3TestCase;
 
 class HelpersTest extends StorageS3TestCase
 {
+    private function executeAssertions($url)
+    {
+        $this->assertNotNull($url);
+        $this->assertIsString($url);
+        $this->assertStringContainsString($this->file, $url);
+
+        $response = (new Client())->request('get', $url);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals(file_get_contents($url), $response->getBody()->getContents());
+    }
+
     /** @test */
     public function file_url_exists()
     {
         $url = s3FileURL($this->file);
 
-        $this->assertNotNull($url);
-        $this->assertIsString($url);
-        $this->assertStringContainsString($this->file, $url);
+        $this->executeAssertions($url);
     }
 
     /** @test */
@@ -22,9 +33,7 @@ class HelpersTest extends StorageS3TestCase
     {
         $url = s3FileUrlTemp($this->file);
 
-        $this->assertNotNull($url);
-        $this->assertIsString($url);
-        $this->assertStringContainsString($this->file, $url);
+        $this->executeAssertions($url);
         $this->assertNotEquals(StorageS3::key($this->file)->url(), $url);
     }
 
